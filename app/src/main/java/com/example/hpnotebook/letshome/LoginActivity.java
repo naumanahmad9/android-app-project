0afc127fcde215c6a773e5e1a4c4e90f058031ea
Nauman Ahmad <tnsd995@gmail.com>
2019-04-04 14:42:44 +0500

    added snackbar

--- app/src/main/java/com/example/hpnotebook/letshome/LoginActivity.java
+++ app/src/main/java/com/example/hpnotebook/letshome/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.hpnotebook.letshome;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,6 +29,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    RelativeLayout login_container;
+    Snackbar snackbar;
     EditText login_email, login_pass;
     Button login_btn;
     ImageView google_signin_btn;
@@ -37,6 +47,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         init();
+
+        testConnection();
+
+        snackbar.setActionTextColor(Color.parseColor("#e6610049"));
+        View view = snackbar.getView();
+        view.setBackgroundColor(Color.WHITE);
+        TextView textView = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.parseColor("#e6610049"));
 
         if(user!=null){
             //startActivity(new Intent(this, Explore.class));
@@ -60,6 +78,44 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void testConnection() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        NetworkCapabilities capabilities = manager.getNetworkCapabilities(manager.getActiveNetwork());
+
+        if (info != null && info.isConnected()) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                showSnackBar(1);
+            } else {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR);
+                showSnackBar(2);
+            }
+        } else {
+            showSnackBar(0);
+        }
+    }
+
+    private void showSnackBar(int check) {
+        if (check == 1) {
+            snackbar = Snackbar.make(container, "Connected to Wifi", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        } else if (check == 2) {
+            snackbar = Snackbar.make(container, "Connected to Mobile Data", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        } else {
+            snackbar = Snackbar.make(container, "No Internet Connection", Snackbar.LENGTH_INDEFINITE);
+            snackbar.show();
+            snackbar.setAction("Retry", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    testConnection();
+                }
+            });
+            snackbar.setActionTextColor(Color.parseColor("#e6610049"));
+        }
+    }
+
     private void authUser(String email, String pass) {
         auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
@@ -73,6 +129,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     private void init() {
+        login_container= findViewById(R.id.login_container);
         login_email=findViewById(R.id.login_email);
         login_pass=findViewById(R.id.login_password);
         login_btn=findViewById(R.id.login_btn);
--- app/src/main/java/com/example/hpnotebook/letshome/SignupActivity.java
+++ app/src/main/java/com/example/hpnotebook/letshome/SignupActivity.java
@@ -1,13 +1,76 @@
 package com.example.hpnotebook.letshome;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    RelativeLayout signup_container;
+    Snackbar snackbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        init();
+
+        testConnection();
+
+        snackbar.setActionTextColor(Color.parseColor("#e6610049"));
+        View view = snackbar.getView();
+        view.setBackgroundColor(Color.WHITE);
+        TextView textView = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.parseColor("#e6610049"));
+    }
+
+    private void testConnection() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        NetworkCapabilities capabilities = manager.getNetworkCapabilities(manager.getActiveNetwork());
+
+        if (info != null && info.isConnected()) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                showSnackBar(1);
+            } else {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR);
+                showSnackBar(2);
+            }
+        } else {
+            showSnackBar(0);
+        }
+    }
+
+    private void showSnackBar(int check) {
+        if (check == 1) {
+            snackbar = Snackbar.make(container, "Connected to Wifi", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        } else if (check == 2) {
+            snackbar = Snackbar.make(container, "Connected to Mobile Data", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        } else {
+            snackbar = Snackbar.make(container, "No Internet Connection", Snackbar.LENGTH_INDEFINITE);
+            snackbar.show();
+            snackbar.setAction("Retry", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    testConnection();
+                }
+            });
+            snackbar.setActionTextColor(Color.parseColor("#e6610049"));
+        }
+    }
+
+    private void init() {
+        signup_container= findViewById(R.id.signup_container);
     }
 }
