a6bf8acb81391c5d35c57519827fb4b4ea55e017
Nauman Ahmad <tnsd995@gmail.com>
2019-04-05 00:31:33 +0500

    made changes in listing item

--- app/src/main/java/com/example/hpnotebook/letshome/ListingViewHolder.java
+++ app/src/main/java/com/example/hpnotebook/letshome/ListingViewHolder.java
@@ -1,4 +1,11 @@
 package com.example.hpnotebook.letshome;
 
-public class ListingViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ListingViewHolder extends RecyclerView.ViewHolder {
+    public ListingViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
 }
