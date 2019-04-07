e631a181c66f57e45afec8019af1cb8d2de48479
Nauman Ahmad <tnsd995@gmail.com>
2019-04-07 19:17:16 +0500

    made some changes

--- app/src/main/java/com/example/hpnotebook/letshome/MainActivity.java
+++ app/src/main/java/com/example/hpnotebook/letshome/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.hpnotebook.letshome;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.hpnotebook.letshome.fragments.ExploreFragment;
+import com.example.hpnotebook.letshome.fragments.FavoritesFragment;
+import com.example.hpnotebook.letshome.fragments.MessagesFragment;
+import com.example.hpnotebook.letshome.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavView_main);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer_main,
+                    new ExploreFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.explore:
+                            selectedFragment = new ExploreFragment();
+                            break;
+                        case R.id.favorites:
+                            selectedFragment = new FavoritesFragment();
+                            break;
+                        case R.id.messages:
+                            selectedFragment = new MessagesFragment();
+                            break;
+                        case R.id.profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer_main,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
