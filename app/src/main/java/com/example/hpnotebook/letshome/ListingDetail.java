340a91897122f7ede230c7ffc4e3f1c1f0f147e1
Nauman Ahmad <tnsd995@gmail.com>
2019-04-10 09:38:57 +0500

    made some changes

--- app/src/main/java/com/example/hpnotebook/letshome/ListingDetail.java
+++ app/src/main/java/com/example/hpnotebook/letshome/ListingDetail.java
@@ -9,5 +9,7 @@ public class ListingDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing_detail);
+
+
     }
 }
--- app/src/main/java/com/example/hpnotebook/letshome/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/hpnotebook/letshome/fragments/ProfileFragment.java
@@ -10,17 +10,23 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.hpnotebook.letshome.R;
+import com.example.hpnotebook.letshome.activities.LoginActivity;
 import com.example.hpnotebook.letshome.activities.SelectListingTypeActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
-    TextView becomeHost;
+    TextView becomeHost, profile_signout;
+    FirebaseAuth auth;
+
+
     public ProfileFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,11 +34,21 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         becomeHost = view.findViewById(R.id.profile_becomeHost);
+        profile_signout = view.findViewById(R.id.profile_signout);
 
         becomeHost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getContext(), SelectListingTypeActivity.class));
+            }
+        });
+
+        profile_signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(getContext(), LoginActivity.class));
+                
             }
         });
         return view;
