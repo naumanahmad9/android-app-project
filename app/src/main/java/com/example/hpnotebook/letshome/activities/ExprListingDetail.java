60a6df4f0b0cab21b98ff70224bc1d99d6b2dbbf
Nauman Ahmad <tnsd995@gmail.com>
2019-04-15 22:21:27 +0500

    made changes

---
+++ app/src/main/java/com/example/hpnotebook/letshome/activities/ExprListingDetail.java
@@ -0,0 +1,13 @@
+package com.example.hpnotebook.letshome.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExprListingDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expr_listing_detail);
+    }
+}
---
+++ app/src/main/java/com/example/hpnotebook/letshome/activities/RestListingDetail.java
@@ -0,0 +1,13 @@
+package com.example.hpnotebook.letshome.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestListingDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_listing_detail);
+    }
+}
