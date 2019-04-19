4b7aa050eddb23e97a4fed115fd8cc1e593489c1
Nauman Ahmad <tnsd995@gmail.com>
2019-04-19 23:58:31 +0500

    added some code

---
+++ app/src/main/java/com/example/hpnotebook/letshome/activities/OnboardingActivity.java
@@ -0,0 +1,14 @@
+package com.example.hpnotebook.letshome.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.hpnotebook.letshome.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
