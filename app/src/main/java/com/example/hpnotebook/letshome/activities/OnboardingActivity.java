47e479a8172570f9a0834ccff21d678073988973
Nauman Ahmad <tnsd995@gmail.com>
2019-04-20 19:26:30 +0500

    deleted some stuff

--- app/src/main/java/com/example/hpnotebook/letshome/activities/OnboardingActivity.java
+++ app/src/main/java/com/example/hpnotebook/letshome/activities/OnboardingActivity.java
@@ -10,5 +10,6 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
     }
 }
